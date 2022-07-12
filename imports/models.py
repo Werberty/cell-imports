6db69acf2935e7e255ba7ddcfeafdf88304029bd
Werberty <werberty.alexandre@gmail.com>
2022-07-11 23:46:03 -0300

    FEAT: Cadastrando produtos e listando-os

--- imports/models.py
+++ imports/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from pyexpat import model
 
-# Create your models here.
+
+class Produto(models.Model):
+    modelo = models.CharField(max_length=65)
+    cor = models.CharField(max_length=35)
+    memoria = models.CharField(max_length=10)
+    armazenamento = models.CharField(max_length=10)
+
+    def __str__(self) -> str:
+        return self.modelo
--- imports/views.py
+++ imports/views.py
@@ -1,7 +1,30 @@
-from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.messages import constants
 from django.shortcuts import render
+
+from .models import Produto
 
 
 def products(request):
     if request.method == 'GET':
+        produtos = Produto.objects.all()
+        return render(request, 'imports/products.html', context={
+            'produtos': produtos,
+        })
+
+    elif request.method == 'POST':
+        modelo = request.POST.get('modelo')
+        cor = request.POST.get('cor')
+        memoria = request.POST.get('memoria')
+        armazenamento = request.POST.get('armazenamento')
+
+        produto = Produto(
+            modelo=modelo,
+            cor=cor,
+            memoria=memoria,
+            armazenamento=armazenamento
+        )
+        produto.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Produto cadastrado')
         return render(request, 'imports/products.html')
