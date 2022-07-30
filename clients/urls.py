0e180796984b99679089d9e3f799c30c5ee7e591
Werberty <werberty.alexandre@gmail.com>
2022-07-30 14:25:24 -0300

    FEAT: editando dados do cliente

--- clients/urls.py
+++ clients/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'clients'
 
 urlpatterns = [
-    path('clients/', views.clients, name='clients')
+    path('clients/', views.clients, name='clients'),
+    path('clients/edit/<int:id_client>', views.edit_client, name='edit_client')
 ]
--- clients/views.py
+++ clients/views.py
@@ -1,15 +1,20 @@
+from ast import Return
+
 from django.contrib import messages
 from django.contrib.messages import constants
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from .forms import ClientesForm
+from .models import Clientes
 
 
 def clients(request):
     if request.method == 'GET':
+        clientes = Clientes.objects.filter(vendedor=request.user)
         form = ClientesForm()
-        return render(request, 'clients.html', context={
-            'form': form
+        return render(request, 'clients/clients.html', context={
+            'form': form,
+            'clientes': clientes,
         })
     if request.method == 'POST':
         form = ClientesForm(request.POST)
@@ -23,6 +28,35 @@ def clients(request):
             messages.add_message(request, constants.ERROR, 'Erro ao cadastrar')
 
         form = ClientesForm()
-        return render(request, 'clients.html', context={
+        return render(request, 'clients/clients.html', context={
             'form': form
+        })
+
+
+def edit_client(request, id_client):
+    cliente = get_object_or_404(Clientes, id=id_client)
+    clientes = Clientes.objects.filter(vendedor=request.user).order_by('-id')
+    form = ClientesForm(instance=cliente)
+    if request.method == 'GET':
+        return render(request, 'clients/edit_client.html', context={
+            'cliente': cliente,
+            'clientes': clientes,
+            'form': form,
+        })
+    if request.method == 'POST':
+        form = ClientesForm(request.POST, instance=cliente)
+
+        if form.is_valid():
+            form.save()
+            messages.add_message(
+                request, constants.SUCCESS, 'Cliente editado')
+        else:
+            messages.add_message(request, constants.ERROR, 'Erro ao editar')
+
+        form = ClientesForm()
+
+        return render(request, 'clients/clients.html', context={
+            'cliente': cliente,
+            'clientes': clientes,
+            'form': form,
         })
