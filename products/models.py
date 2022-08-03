3a83d442d9f7459397827dcfffef4eae13e194a4
Werberty <werberty.alexandre@gmail.com>
2022-08-03 07:31:21 -0300

    FEAT: data de criação e adição de produto

--- products/models.py
+++ products/models.py
@@ -1,3 +1,4 @@
+from clients.models import Clientes
 from django.contrib.auth.models import User
 from django.db import models
 
@@ -12,20 +13,25 @@ class Produto(models.Model):
     )
 
     ARMAZ_CHOICES = (
-        (32, '32Gb RAM'),
-        (64, '64Gb RAM'),
-        (128, '128Gb RAM'),
-        (256, '256Gb RAM'),
-        (512, '512Gb RAM'),
+        (16, '16Gb ROM'),
+        (32, '32Gb ROM'),
+        (64, '64Gb ROM'),
+        (128, '128Gb ROM'),
+        (256, '256Gb ROM'),
+        (512, '512Gb ROM'),
     )
 
     vendedor = models.ForeignKey(
         User, on_delete=models.CASCADE, default='')
+    # comprador = models.ForeignKey(
+    #     Clientes, on_delete=models.CASCADE, blank=True)
     marca = models.CharField(max_length=45)
     modelo = models.CharField(max_length=65)
     cor = models.CharField(max_length=35)
     memoria = models.IntegerField(choices=MEMORIA_CHOICES)
     armazenamento = models.IntegerField(choices=ARMAZ_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
         return self.modelo
