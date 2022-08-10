0c9340fcf135e4b95b99f1cce117f109c4b63c51
Werberty <werberty.alexandre@gmail.com>
2022-08-09 21:40:51 -0300

    TEST: corrigindo urls novas

--- products/tests/test_product_urls.py
+++ products/tests/test_product_urls.py
@@ -5,16 +5,16 @@ from django.urls import reverse
 class ProductUrlsTest(TestCase):
     def test_home_url_is_correct(self):
         url = reverse('products:home')
-        self.assertEqual(url, '/')
+        self.assertEqual(url, '/stock/')
 
     def test_product_url_is_correct(self):
         url = reverse('products:products')
-        self.assertEqual(url, '/products/')
+        self.assertEqual(url, '/stock/products/')
 
     def test_delete_product_url_is_correct(self):
         url = reverse('products:delete_product', kwargs={'product_id': 1})
-        self.assertEqual(url, '/products/delete/1')
+        self.assertEqual(url, '/stock/products/delete/1')
 
     def test_edit_product_url_is_correct(self):
         url = reverse('products:edit_product', kwargs={'product_id': 1})
-        self.assertEqual(url, '/products/edit/1')
+        self.assertEqual(url, '/stock/products/edit/1')
