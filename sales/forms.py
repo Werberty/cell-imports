d4ebb001eeff3c68d8db294dec7c8dcf963b45a5
Werberty <werberty.alexandre@gmail.com>
2022-12-07 22:09:02 -0300

    FEAT: Filtrando somento produtos em stock na venda

--- sales/forms.py
+++ sales/forms.py
@@ -1,20 +1,8 @@
 from django import forms
-from django.core.exceptions import ValidationError
 
 from products.models import Produto
 
 from .models import Venda
-
-produtos = Produto.objects.filter(vendido=False)
-
-
-def queryset_to_tuple_list(queryset):
-    result = []
-    for query in queryset.values():
-        result.append(
-            ((query['id']), (f"{query['marca']} {query['modelo']}"))
-        )
-    return result
 
 
 class VendasForm(forms.ModelForm):
@@ -23,7 +11,10 @@ class VendasForm(forms.ModelForm):
         self.fields['observacoes'].widget.attrs.update(
             {'style': 'height: 70px;'})
 
-    produto = forms.ChoiceField(choices=queryset_to_tuple_list(produtos))
+    produto = forms.ModelChoiceField(
+        label='Produto',
+        queryset=Produto.objects.filter(vendido=False)
+    )
 
     class Meta:
         model = Venda
